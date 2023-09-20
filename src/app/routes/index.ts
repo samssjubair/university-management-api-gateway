077bbf329719422b5ba80d62572fc79773c2ffad
samssjubair <samssjubair@gmail.com>
2023-09-20 23:57:53 +0600

    first commit

---
+++ src/app/routes/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { academicSemesterRoutes } from '../modules/academicSemester/academicSemester.routes';
+
+const router = express.Router();
+
+const moduleRoutes = [
+  {
+    path: '/academic-semesters',
+    routes: academicSemesterRoutes
+  }
+];
+
+moduleRoutes.forEach((route) => {
+  router.use(route.path, route.routes);
+});
+
+export default router;
---
+++ src/shared/redis.ts
@@ -0,0 +1,25 @@
+import { createClient } from "redis";
+import logger from "./logger";
+import config from "../config";
+
+let redisClient= createClient({
+    url: config.redis.url,
+})
+
+redisClient.on('error', (err) => {
+    logger.error('Redis Error: ',err);
+}   
+);
+
+redisClient.on('connect', () => {
+    logger.info('Redis is connected');
+}   
+);
+
+const connect= async (): Promise<void> => {
+    await redisClient.connect();
+}
+
+export const RedisClient= {
+    connect,
+};
